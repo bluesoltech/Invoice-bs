1d9f51a7484b3cafe5f19be4ee1bbeeb86241495
bluesoltech-kunal <dev.kunal.bluesoltech@gmail.com>
2024-01-25 13:00:03 +0530

    Prepare Item List

--- client/src/assets/data/ItemList.js
+++ client/src/assets/data/ItemList.js
@@ -3,16 +3,67 @@ const data = [
     value: "Sports",
     symbol: "Sports",
     subcategory: [
-      { value: "BIB Expo & Adhere", symbol: "BIB Expo & Adhere" },
-      { value: "Promo Run", symbol: "Promo Run" },
+      {
+        value: "BIB Expo & Adhere",
+        symbol: "BIB Expo & Adhere",
+        subcategory: [
+          {
+            value: "All Licenses and Permissions",
+            symbol: "All Licenses and Permissions",
+            items: [{}],
+          },
+          {
+            value: "Barricading & Adhere",
+            symbol: "Barricading & Adhere",
+            items: [{}],
+          },
+          { value: "Venue Branding", symbol: "Venue Branding", items: [{}] },
+        ],
+      },
+      {
+        value: "Promo Run",
+        symbol: "Promo Run",
+        subcategory: [
+          {
+            value: "All Licenses and Permissions",
+            symbol: "All Licenses and Permissions",
+            items: [{}],
+          },
+          {
+            value: "Barricading & Adhere",
+            symbol: "Barricading & Adhere",
+            items: [{}],
+          },
+        ],
+      },
     ],
   },
   {
     value: "Social",
     symbol: "Social",
     subcategory: [
-      { value: "Haldi", symbol: "Haldi" },
-      { value: "Sangeet", symbol: "Sangeet" },
+      {
+        value: "Haldi",
+        symbol: "Haldi",
+        subcategory: [
+          {
+            value: "All Items",
+            symbol: "All Items",
+            items: [{}],
+          },
+        ],
+      },
+      {
+        value: "Sangeet",
+        symbol: "Sangeet",
+        subcategory: [
+          {
+            value: "All Items",
+            symbol: "All Items",
+            items: [{}],
+          },
+        ],
+      },
     ],
   },
   { value: "Others", symbol: "Others" },
--- client/src/components/Item/Item.jsx
+++ client/src/components/Item/Item.jsx
@@ -5,14 +5,17 @@ import data from "../../assets/data/ItemList";
 function Item() {
   const [category, setCategory] = useState("Sports");
   let subData = data.find((item) => item.value === category).subcategory;
+
+  console.log(subData);
+
   const [subcategory, setSubCategory] = useState(subData[0].value);
-  useEffect(() => {
-    setSubCategory(subData[0].value);
-    subData = data.find((item) => item.value === category).subcategory;
-  }, [category]);
-  //   const handleInputChange = () => {};
+  console.log(subcategory);
+
+  let subClass = subData.find((item) => item.value === subcategory).subcategory;
+  const [subclass, setSubClass] = useState(subClass[0].value);
+
   return (
-    <div className="w-full h-auto bg-gray-100 p-3">
+    <div className="w-full h-auto bg-gray-50 border-[1px] rounded p-3">
       <label className="font-bold text-md ">Category:</label>
       <div className="px-3 py-1">
         <Dropdown data={data} title={category} setData={setCategory} />
@@ -20,6 +23,10 @@ function Item() {
       <label className="font-bold text-md ">Sub Category:</label>
       <div className="px-3 py-1">
         <Dropdown data={subData} title={subcategory} setData={setSubCategory} />
+      </div>
+      <label className="font-bold text-md ">Sub Class:</label>
+      <div className="px-3 py-1">
+        <Dropdown data={subClass} title={subclass} setData={setSubClass} />
       </div>
     </div>
   );
